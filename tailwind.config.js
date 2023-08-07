53c2761f65f4f2716b365498d0a9aecc18f9b3d8
Hamid <ammaraslammuhammad@gmail.com>
2023-08-07 19:25:17 +0500

    \

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
-    
     extend: {
       colors: {
-        primary: '#0F3A56',
-        accent: '#5CBDFA',
-        secondary: '#EFF8FF'
-      }
+        "primary": "#fe5368",
+        "primary-dark": "rgb(27 63 87)",
+        "silver-light":" rgb(232 236 238)",
+        "light-green":" rgb(76 130 130)"
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+        animation: {
+          "accordion-down": "accordion-down 0.2s ease-out",
+          "accordion-up": "accordion-up 0.2s ease-out",
+        },
+      },
+
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
