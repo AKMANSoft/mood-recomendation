d5c205e90d58eea45cb4a14ac4d2267af20442f3
Ali Hussnain <alihussnainrrb@gmail.com>
2023-10-31 14:05:35 +0500

    added build to gitignore

--- build/assets/HeaderComponent-de41a190.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-fcaf22c8.js";function t(){return e.jsx("header",{children:e.jsx("div",{className:"flex  md:justify-end justify-center items-center pt-5 md:pr-20 bg-primary",children:e.jsx("a",{href:"#",children:e.jsx("img",{src:"/images/logo.png",className:"object-contain object-center h-7 md:h-12 w-auto",alt:""})})})})}export{t as H};
